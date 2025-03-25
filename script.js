e0c6716dfa6b7bd0ff1553b6395858bc0bfa2f28
Kristina <kgritsenko.2017@gmail.com>
2025-03-25 16:59:52 +0200

    feat:start

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(bth => bth.addEventListener('click', openModal))
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+///////////////////////////////////////
+const header = document.querySelector('.header');
+
+//Creating and inserting element
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookied for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+  // header.prepend(message);
+  header.append(message);
+  // header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+//Delete 
+
+document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+  message.remove();
+})
